a7af74cc83cc7b7d8c793a68271fe1f1956cb31e
jorelm68 <methan@umich.edu>
2024-09-29 06:46:10 -0400

    fa;dfjasdf

--- routes/spotify.js
+++ routes/spotify.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const {
     getTrack,
+    getProgress,
 } = require('../controllers/spotifyController');
 
 router.post('/getTrack', getTrack);
+router.post('/getProgress', getProgress);
 
 module.exports = router;
